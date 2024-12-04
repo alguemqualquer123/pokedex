3120003d5ded5cb69fbc9ad321032c2a54c1a981
Dev` Juniors <136682675+alguemqualquer123@users.noreply.github.com>
2024-12-04 04:07:52 -0300

    a

--- src/store/pokemonStore.ts
+++ src/store/pokemonStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { Pokemon } from '../types/pokemon';
 
 interface PokemonStore {
@@ -8,19 +9,53 @@ interface PokemonStore {
   isInCollection: (pokemonId: number) => boolean;
 }
 
-export const usePokemonStore = create<PokemonStore>((set, get) => ({
-  collection: [],
-  addToCollection: (pokemon) => {
-    set((state) => ({
-      collection: [...state.collection, pokemon],
-    }));
-  },
-  removeFromCollection: (pokemonId) => {
-    set((state) => ({
-      collection: state.collection.filter((p) => p.id !== pokemonId),
-    }));
-  },
-  isInCollection: (pokemonId) => {
-    return get().collection.some((p) => p.id === pokemonId);
-  },
-}));
+export const usePokemonStore = create<PokemonStore>()(
+  persist(
+    (set, get) => ({
+      collection: [],
+      addToCollection: (pokemon) => {
+        set((state) => ({
+          collection: [...state.collection, pokemon],
+        }));
+      },
+      removeFromCollection: (pokemonId) => {
+        set((state) => ({
+          collection: state.collection.filter((p) => p.id !== pokemonId),
+        }));
+      },
+      isInCollection: (pokemonId) => {
+        return get().collection.some((p) => p.id === pokemonId);
+      },
+    }),
+    {
+      name: 'pokemon-collections', // Nome da chave no LocalStorage
+    }
+  )
+);
+
+// import { create } from 'zustand';
+// import { Pokemon } from '../types/pokemon';
+
+// interface PokemonStore {
+//   collection: Pokemon[];
+//   addToCollection: (pokemon: Pokemon) => void;
+//   removeFromCollection: (pokemonId: number) => void;
+//   isInCollection: (pokemonId: number) => boolean;
+// }
+
+// export const usePokemonStore = create<PokemonStore>((set, get) => ({
+//   collection: [],
+//   addToCollection: (pokemon) => {
+//     set((state) => ({
+//       collection: [...state.collection, pokemon],
+//     }));
+//   },
+//   removeFromCollection: (pokemonId) => {
+//     set((state) => ({
+//       collection: state.collection.filter((p) => p.id !== pokemonId),
+//     }));
+//   },
+//   isInCollection: (pokemonId) => {
+//     return get().collection.some((p) => p.id === pokemonId);
+//   },
+// }));
